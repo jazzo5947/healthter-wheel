da7b8efc10ca5654551a5b28a2190df8c71927a1
jazzo5947 <jazzo5947@gmail.com>
2022-04-05 09:57:42 +0900

    first commit

---
+++ fe/landcalc/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #e9ecef;
+  }
+`;
+
+function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <TodoTemplate>
+                <TodoHead></TodoHead>
+                <TodoList></TodoList>
+            </TodoTemplate>
+        </>
+    );
+}
+
+export default App;
